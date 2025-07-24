49711c6c147183a9b6535ee564834425efc551d4
Amir Dahal <amirdahal04@gmail.com>
2025-07-24 11:39:35 +0530

    Open evacuation center positions on map. Share evacuation center location.

--- lib/src/features/map/widgets/evacuation_center_layer.widget.dart
+++ lib/src/features/map/widgets/evacuation_center_layer.widget.dart
@@ -1,7 +1,13 @@
+import 'dart:io';
+
 import 'package:aprs/src/model/model.dart';
+import 'package:aprs/src/widgets/toast.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:share_plus/share_plus.dart';
+import 'package:toastification/toastification.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class EvacuationCenterLayer extends StatefulWidget {
   final MapController mapController;
@@ -12,13 +18,25 @@ class EvacuationCenterLayer extends StatefulWidget {
 }
 
 class _EvacuationCenterLayerState extends State<EvacuationCenterLayer> {
-
   List<EvacuationCentre> _centers = [];
 
-  void loadEvacuationCenters() async{
-    _centers =  await EvacuationCentre().select().toList();
+  void loadEvacuationCenters() async {
+    _centers = await EvacuationCentre().select().toList();
     setState(() {});
     widget.mapController.move(widget.mapController.camera.center, 8);
+  }
+
+  void _launchUrl(Uri url) async {
+    if (await canLaunchUrl(url)) {
+      await launchUrl(url, mode: LaunchMode.externalApplication);
+    } else {
+      showToast(
+        context: context,
+        title: 'Get direction',
+        description: 'Could not launch maps',
+        type: ToastificationType.error,
+      );
+    }
   }
 
   @override
@@ -29,16 +47,83 @@ class _EvacuationCenterLayerState extends State<EvacuationCenterLayer> {
 
   @override
   Widget build(BuildContext context) {
-    return MarkerLayer(markers: [
-      for(var center in _centers)
-        Marker(
-          point: LatLng(center.latitude!, center.longitude!),
-          child: InkWell(
+    return MarkerLayer(
+      markers: [
+        for (var center in _centers)
+          Marker(
+            point: LatLng(center.latitude!, center.longitude!),
+            child: InkWell(
               onTap: () {
-                widget.mapController.move( LatLng(center.latitude!, center.longitude!), 10);
+                showModalBottomSheet(
+                  context: context,
+                  builder: (context) {
+                    return Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        ListTile(
+                          trailing: Icon(Icons.location_on_outlined),
+                          title: Text(center.name ?? ''),
+                          subtitle: Text(center.address ?? ''),
+                        ),
+                        ListTile(
+                          trailing: Icon(Icons.map_outlined),
+                          title: Text('Get direction'),
+                          onTap: () async {
+                            String googleMapsUrl =
+                                'https://www.google.com/maps/search/?api=1&query=${center.latitude},${center.longitude}';
+                            String appleMapsUrl =
+                                'https://maps.apple.com/?q=${center.latitude},${center.longitude}';
+
+                            final Uri url = Uri.parse(
+                              Platform.isIOS || Platform.isMacOS
+                                  ? appleMapsUrl
+                                  : googleMapsUrl,
+                            );
+                            _launchUrl(url);
+                          },
+                        ),
+                        ListTile(
+                          title: Text('Share with others'),
+                          trailing: Icon(Icons.share),
+                          onTap: () async {
+                            String googleMapsUrl =
+                                'https://www.google.com/maps/search/?api=1&query=${center.latitude},${center.longitude}';
+                            String appleMapsUrl =
+                                'https://maps.apple.com/?q=${center.latitude},${center.longitude}';
+
+                            final Uri url = Uri.parse(
+                              Platform.isIOS || Platform.isMacOS
+                                  ? appleMapsUrl
+                                  : googleMapsUrl,
+                            );
+                            final result = await SharePlus.instance.share(
+                              ShareParams(
+                                title: '${center.name} ${center.address}',
+                                subject: '${center.name} ${center.address}',
+                                text:
+                                    'I am sharing an address to the evacuation center named "${center.name}" located at "${center.address}". Use the provided url to reach there.',
+                                uri: url,
+                              ),
+                            );
+                            if (result.status == ShareResultStatus.success) {
+                              showToast(
+                                context: context,
+                                title: 'Shared evacuation center location',
+                                description: '',
+                              );
+                            }
+                          },
+                        ),
+                      ],
+                    );
+                  },
+                );
+                // widget.mapController.move( LatLng(center.latitude!, center.longitude!), 10);
               },
-              child: Image.asset('assets/images/evacuation.png')),
-        ),
-    ]);
+              child: Image.asset('assets/images/evacuation.png'),
+            ),
+          ),
+      ],
+    );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,8 +10,10 @@ import geolocator_apple
 import package_info_plus
 import path_provider_foundation
 import screen_retriever_macos
+import share_plus
 import sqflite_sqlcipher
 import sqlite3_flutter_libs
+import url_launcher_macos
 import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -20,7 +22,9 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SqfliteSqlCipherPlugin.register(with: registry.registrar(forPlugin: "SqfliteSqlCipherPlugin"))
   Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
   WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
