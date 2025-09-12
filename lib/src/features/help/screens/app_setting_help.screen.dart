6bbac11272c0f77870fca0f629ea53d37395a13d
Amir Dahal <amirdahal04@gmail.com>
2025-09-12 22:01:10 +0530

    app setting guide

--- lib/src/features/help/screens/app_setting_help.screen.dart
+++ lib/src/features/help/screens/app_setting_help.screen.dart
@@ -1,3 +1,4 @@
+import 'package:drr_radio_tracker/src/features/help/widgets/builder.widget.dart';
 import 'package:flutter/material.dart';
 
 import '../constants/help.mixin.dart';
@@ -7,7 +8,21 @@ class AppSettingHelpScreen extends StatelessWidget with HelpPageMixin {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ListView(
+      padding: EdgeInsets.all(16),
+      children: [
+        BuildSection(
+          title: "App settings",
+          content:
+              "This setting allows you to update your validation password.\n"
+              "A validation password is required to prevent accidental misconfigurations by random touches.",
+        ),
+
+        const Divider(height: 16),
+
+        BuildSection(title: "Updating password", content: "In the provided form, enter a new password and save the setting.\n" "The default password is '123456'.")
+      ],
+    );
   }
 
   @override
